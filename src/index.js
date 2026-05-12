1db2119a1d54a67121f1a9945c4424d6b3645dad
RayCodes50 <przemox2@gmail.com>
2026-05-12 12:16:26 +0100

    Create HTML template and css style

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import "./styles.css";
 import { getWeather } from "./weatherApi.js";
 import { setWeatherData, getWeatherData } from "./weatherState.js";
+//import svg this way
+// import clearDay from "@meteocons/svg/fill/clear-day.svg";
 
+// const weatherSum = document.getElementById("weatherSum");
 // the way to implement svg
-import clearDay from "@meteocons/svg/fill/clear-day.svg";
 let units = "US";
 const result = await getWeather("MaNchESTER", units);
 console.log(result);
 setWeatherData(result);
 console.log(getWeatherData().currentConditions);
 
-// console.log(result.currentConditions);
+// // console.log(result.currentConditions);
 
-const img = document.createElement("img");
-img.src = clearDay;
-img.width = 100;
-img.height = 100;
-img.alt = "Clear day";
-document.body.appendChild(img);
+// const img = document.createElement("img");
+// img.src = clearDay;
+// img.width = 180;
+// img.height = 180;
+// img.alt = "Clear day";
+// weatherSum.appendChild(img);
 // end
